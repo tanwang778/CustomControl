fa41a62ba302b8fbee53865cff593dcfa5f38e37
战神谭 <2764732335@qq.com>
2023-04-10 11:44:25 +0800

    更改成了视图绑定方式

--- app/src/main/java/com/example/customcontrol/TitleLayout.kt
+++ app/src/main/java/com/example/customcontrol/TitleLayout.kt
@@ -7,19 +7,28 @@ import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.Toast
+import com.example.customcontrol.databinding.TitleBinding
 
+/**
+ * @param context 上下文
+ * @param attrs 样式
+ */
 class TitleLayout(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
-    private lateinit var titleBack: Button
-    private lateinit var titleEdit: Button
+    //    private lateinit var titleBack: Button
+//    private lateinit var titleEdit: Button
+    private lateinit var binding: TitleBinding
+
     init {
+        //动态加载布局
         LayoutInflater.from(context).inflate(R.layout.title, this)
-        titleBack = findViewById(R.id.titleBack)
-        titleEdit = findViewById(R.id.titleEdit)
-        titleBack.setOnClickListener {
+        binding = TitleBinding.bind(this)
+//        titleBack = findViewById(R.id.titleBack)
+//        titleEdit = findViewById(R.id.titleEdit)
+        binding.titleBack.setOnClickListener {
             val activity = context as Activity
             activity.finish()
         }
-        titleEdit.setOnClickListener {
+        binding.titleEdit.setOnClickListener {
             Toast.makeText(context, "You Clicked Edit Button", Toast.LENGTH_SHORT).show()
         }
     }
