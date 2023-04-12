798521fb4de309c891bd1110a8de0044d61bccb6
战神谭 <2764732335@qq.com>
2023-04-12 08:36:29 +0800

    实现了+1功能

--- app/src/main/java/com/example/customcontrol/MainActivity.kt
+++ app/src/main/java/com/example/customcontrol/MainActivity.kt
@@ -6,15 +6,21 @@ import android.widget.Toast
 import com.example.customcontrol.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private var num = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
         binding.titleLayout.setTitle("首页")
-        binding.titleLayout.setEdit{
+        binding.titleLayout.setEdit {
             Toast.makeText(this, "You Clicked Edit Button", Toast.LENGTH_SHORT).show()
+        }
+        //+1事件
+        binding.mainBtnAdd.setOnClickListener {
+            num++
+            binding.mainTextNum.text = num.toString()
         }
     }
 
